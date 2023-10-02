aeb423e35fc9bd150b84d5d87b4b91bc1d07aba9
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-02 20:30:16 -0300

    Comentários no Kotlin

---
+++ src/main/kotlin/br/com/rayana/section04/_0404/Comments.kt
@@ -0,0 +1,36 @@
+package br.com.rayana.section04._0404
+
+fun main() {
+    // I am a comment
+   // val myPhrases = "I'm a String"
+    val myPi= 3.4
+    val myAnswer = 42
+
+    /*
+    I am a comment too
+    I am a block comment
+    val aString: String
+    val aDouble: Double
+    val aInt: Int
+     */
+    val myDouble = 1.999
+    val myFloat = 1.9F
+    val myLong = 998420399L
+    val myInt = 199
+    val myShort: Short = 12
+    val myByte: Byte = 127
+
+    val aLongNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong = anInt.toLong()
+}
+
+/*
+fun fooBar() {
+    /*
+    I am embedded comment
+     */
+}
+ */
