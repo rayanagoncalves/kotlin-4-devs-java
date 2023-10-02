def47c6dd115313e219127db3b32b0e43a72e99a
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-02 20:36:15 -0300

    Expressões If no Kotlin

---
+++ src/main/kotlin/br/com/rayana/section04/_405/IfExpression.kt
@@ -0,0 +1,19 @@
+package br.com.rayana.section04._405
+
+fun main() {
+    val myInt = 199
+    val aInt = 7
+
+   // int lowest = (myInt < aInt) ? myInt : aInt em java
+    val lowest = if (myInt < aInt) myInt else aInt
+
+    val temp = 26
+    val isAirConditionerOn = if(temp >= 26) {
+        println("It's warm")
+        true
+    } else {
+        println("It's not so warm")
+        false
+    }
+    println("Is the air conditioner on: $isAirConditionerOn")
+}
---
+++ src/main/kotlin/br/com/rayana/section04/_405/main.kt
@@ -0,0 +1,25 @@
+package br.com.rayana.section04._405
+
+fun main() {
+    val myPhrases = "I'm a String"
+    val myPi= 3.4
+    val myAnswer = 42
+
+    val aString: String
+    val aDouble: Double
+    val aInt: Int
+
+    val myDouble = 1.999
+    val myFloat = 1.9F
+    val myLong = 998420399L
+    val myInt = 199
+    val myShort: Short = 12
+    val myByte: Byte = 127
+
+    val aLongNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong = anInt.toLong()
+
+}
