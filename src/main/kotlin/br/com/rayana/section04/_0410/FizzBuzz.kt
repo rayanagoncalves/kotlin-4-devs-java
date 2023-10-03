402a9772bc5b1d583059a136f418953cee0be3e5
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-02 21:36:44 -0300

    Implementando FizzBuzz usando forEach

--- src/main/kotlin/br/com/rayana/section04/_0410/FizzBuzz.kt
+++ src/main/kotlin/br/com/rayana/section04/_0410/FizzBuzz.kt
@@ -1,6 +1,6 @@
 package br.com.rayana.section04._0410
 
-fun fizzBuzz(n: Int): List<String> {
+fun fizzBuzz1(n: Int): List<String> {
     val list = mutableListOf<String>()
 
     for(it in 1 .. n) {
@@ -14,6 +14,22 @@ fun fizzBuzz(n: Int): List<String> {
 //            list.add(it.toString())
 //        }
 
+        val item = when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
+        }
+        list.add(item)
+    }
+
+    return list
+}
+
+fun fizzBuzz(n: Int): List<String> {
+    val list = mutableListOf<String>()
+
+    (1 .. n).forEach() {
         val item = when {
             it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
             it % 3 == 0 -> "Fizz"
