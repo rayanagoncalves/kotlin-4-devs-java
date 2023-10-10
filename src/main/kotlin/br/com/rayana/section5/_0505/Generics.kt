fe7f69c5b92022bed52428806eeb3378dd4fbb08
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-09 22:18:20 -0300

    Trabalhando com Generics

---
+++ src/main/kotlin/br/com/rayana/section5/_0505/Generics.kt
@@ -0,0 +1,14 @@
+package br.com.rayana.section5._0505
+
+fun main() {
+    val maxInt = max(42, 49)
+    val maxLong = max(123_456_789L, 999_999_999L)
+    val maxByte = max((-128).toByte(), (127).toByte())
+    val maxString = max("Alpha", "Omega")
+
+    println("The max Int = $maxInt")
+    println("The max Long = $maxLong")
+    println("The max Byte = $maxByte")
+    println("The max String = $maxString")
+
+}
---
+++ src/main/kotlin/br/com/rayana/section5/_0505/Max.kt
@@ -0,0 +1,7 @@
+package br.com.rayana.section5._0505
+
+fun <T: Comparable<T>> max(param1: T, param2: T): T {
+
+    val result = param1.compareTo(param2)
+    return if(result > 0) param1 else param2
+}
