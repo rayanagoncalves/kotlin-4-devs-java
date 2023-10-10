50f19b2a9595fd3dab203785640fe28197ce279a
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-10 20:04:28 -0300

    Interoperabilidade com Java: Usando Java no Kotlin

---
+++ src/main/kotlin/br/com/rayana/section06/_0602/Hello.kt
@@ -0,0 +1,11 @@
+package br.com.rayana.section06._0602
+
+fun main() {
+    val cat = Animal("Garfield", "cat", 10)
+    println(cat.show())
+    val dog = Animal("Totó", "dog", 30)
+    println(dog.show())
+
+    cat.weight = 13
+    println(cat.show())
+}
