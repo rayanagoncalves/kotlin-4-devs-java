ea3a3e3dbb0230712c1a918a939d90c77c511247
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-12 19:48:38 -0300

    Programação Funcional em Kotlin: Currying

---
+++ src/main/kotlin/br/com/rayana/section08/_0808/Currying.kt
@@ -0,0 +1,11 @@
+package br.com.rayana.section08._0808
+
+import org.funktionale.currying.curried
+
+fun main() {
+    val sum3ints = {x: Int, y: Int, z: Int -> x + y + z}
+
+    val curried: (Int) -> (Int) -> (Int) -> Int = sum3ints.curried()
+    val curriedResult = curried(2)(4)(6)
+    println("curried(2)(4)(6) = $curriedResult")
+}
