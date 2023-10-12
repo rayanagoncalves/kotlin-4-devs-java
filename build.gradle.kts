3be6feb7b8191b37de2f06869fdf393a66d491b8
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-12 19:39:14 -0300

    Programação Funcional em Kotlin: adicionando a dependência da lib funKtionale

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://jcenter.bintray.com")
 }
 
 dependencies {
+    implementation("org.funktionale:funktionale-all:1.2")
     runtimeOnly ("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+
     testImplementation(kotlin("test"))
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testImplementation ("org.amshove.kluent:kluent:1.73")
